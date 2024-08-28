aa5adbeb7f801013de339cbab529f5f0a9263ebe
silveyratomas <85298972+silveyratomas@users.noreply.github.com>
2024-08-28 17:42:46 -0300

    Create Profesor.java

---
+++ Profesor.java
@@ -0,0 +1,13 @@
+public class Profesor {
+    private String nombre;
+    private String dni;
+
+    public Profesor(String nombre, String dni) {
+        this.nombre = nombre;
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
