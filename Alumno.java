b603230596def2ad7660d1891e244e300faa0132
silveyratomas <85298972+silveyratomas@users.noreply.github.com>
2024-08-30 16:20:20 -0300

    Update Alumno.java

--- Alumno.java
+++ Alumno.java
@@ -1,4 +1,5 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Alumno {
     private String nombre;
@@ -13,47 +14,27 @@ public class Alumno {
         this.materias = new HashMap<>();
     }
 
-    public boolean estaInscritoEn(Materia materia) {
-        return materias.containsKey(materia);
+    public void inscribirMateria(Materia materia) {
+        materias.put(materia, null);
     }
 
-    public String getDni(){
-        return dni;
+    public boolean estaInscritoEn(Materia materia) {
+        return materias.containsKey(materia);
     }
 
     public String getNombre() {
         return nombre;
     }
 
+    public String getDni() {
+        return dni;
+    }
+
     public int getLegajo() {
         return legajo;
     }
 
-    public void inscribirMateria(Materia materia) {
-        materias.put(materia, null);
-    }
-
-    public void registrarSituacionFinal(Materia materia, SituacionFinal situacion) {
-        if (materias.containsKey(materia)) {
-            materias.put(materia, situacion);
-        } else {
-            System.out.println("El alumno no está inscrito en la materia " + materia.getNombre());
-        }
-    }
-
-    public void mostrarMaterias() {
-        for (Map.Entry<Materia, SituacionFinal> entry : materias.entrySet()) {
-            Materia materia = entry.getKey();
-            SituacionFinal situacion = entry.getValue();
-            System.out.println("Materia: " + materia.getNombre() + " - Curso: " + materia.getCurso() +
-                    " - Cuatrimestre: " + materia.getCuatrimestre() +
-                    " - Profesor: " + materia.getProfesor().getNombre());
-            if (situacion != null) {
-                System.out.println("Situación final: " + situacion.getEstado() +
-                        " - Inasistencias: " + situacion.getInasistencias());
-            } else {
-                System.out.println("El alumno está cursando esta materia.");
-            }
-        }
+    public Map<Materia, SituacionFinal> getMaterias() {
+        return materias;
     }
 }
